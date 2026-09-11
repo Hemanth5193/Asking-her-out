593982ef12306dad5c9ee91b9460623ecdf4788d
Hemanth5193 <117073442+Hemanth5193@users.noreply.github.com>
2026-09-11 22:46:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -352,7 +352,7 @@ function attachPetalToFlower(quality, index) {
     const angleDeg = index * 72 - 90;
 
     // Radius from center
-    const radius = window.innerWidth > 480 ? 75 : 55;
+    const radius = window.innerWidth > 480 ? 60 : 45;
 
     // Precise rotational placement extending from center
     petalWrap.style.left = `calc(50% - ${window.innerWidth > 480 ? 40 : 32}px)`;
